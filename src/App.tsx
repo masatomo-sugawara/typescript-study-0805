685fc0cb7e35afedb35bed240c41e04b6c674748
masatomo-sugawara <masatomo.s1982@gmail.com>
2022-08-05 17:21:53 +0900

    59. 補足(ライブラリの型定義について)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import { useState } from "react";
 import { Todo } from "./Todo";
 import "./styles.css";
 import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "まさとも",
+  hobbies: ["テニス", "パソコン"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -15,6 +23,8 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color={"red"} fontSize={"18px"} />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todos) => (
         <Todo
